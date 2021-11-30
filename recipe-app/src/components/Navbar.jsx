a0fc9bb3fe36e40104882f5b714aee82467d5b3b
Vivek Thakore <vivek.thakore@waitrapp.com>
2021-11-30 16:22:52 -0600

    updated navbar, added components

--- recipe-app/src/components/Navbar.jsx
+++ recipe-app/src/components/Navbar.jsx
@@ -1,52 +1,14 @@
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar'
+import {Link} from 'react-router-dom';
 
-function Navbar() {
-    return (
-        <div>
-            <Navbar bg="light" expand={false}>
-  <Container fluid>
-    <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
-    <Navbar.Toggle aria-controls="offcanvasNavbar" />
-    <Navbar.Offcanvas
-      id="offcanvasNavbar"
-      aria-labelledby="offcanvasNavbarLabel"
-      placement="end"
-    >
-      <Offcanvas.Header closeButton>
-        <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
-      </Offcanvas.Header>
-      <Offcanvas.Body>
-        <Nav className="justify-content-end flex-grow-1 pe-3">
-          <Nav.Link href="#action1">Home</Nav.Link>
-          <Nav.Link href="#action2">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
-            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action5">
-              Something else here
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Form className="d-flex">
-          <FormControl
-            type="search"
-            placeholder="Search"
-            className="me-2"
-            aria-label="Search"
-          />
-          <Button variant="outline-success">Search</Button>
-        </Form>
-      </Offcanvas.Body>
-    </Navbar.Offcanvas>
-  </Container>
-</Navbar>
+function Navbar(props) {
+    return(
+        <div className="Navbar" style={{"display":"flex","justifyContent":"space-evenly"}} >
+            <Link to="/home">App Title</Link>
+            <Link to="/favorite">Favorites</Link>
+            <Link to="/aboutus">About Us</Link>
         </div>
-    )
+    );
 }
 
 
-
-
-export default Navbar;
+export default Navbar
