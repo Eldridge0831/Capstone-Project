e94d11c94bdd59bfc44e18e4f9c97915816ee14c
Charles Eldridge <charleseldridge@Charless-MacBook-Pro.local>
2021-12-02 21:25:16 -0500

    display and recipe cards with routing

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home';
 import Favorite from './components/Favorite';
 import Aboutus from './components/Aboutus';
 import Page404 from './components/Page404';
+import RecipeCard from './components/RecipeCard';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -19,13 +20,14 @@ function App() {
     <div className="App">
       <Router>
         <Navbar />
-        <RecipeSearch />
         <Routes>
           <Route path = "/home" element={<Home />} />
+          <Route path = "/search" element={<RecipeSearch />} />
           <Route path = "/favorite" element={<Favorite />} />
           <Route path = "/404" element={<Page404 />} />
           <Route path = "/nutrition" element={<NutritionData />} />
           <Route path = "/aboutus" element={<Aboutus />} />
+          <Route path = "/recipe" element={<RecipeCard />} />
         </Routes>
       </Router> 
     </div>
