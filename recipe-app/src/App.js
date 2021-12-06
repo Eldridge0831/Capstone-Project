cb3fbc130627be061d6e8b65ad19e7ec23a6a815
Eldridge0831 <85918493+Eldridge0831@users.noreply.github.com>
2021-12-05 21:11:45 -0500

    added backend

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import RecipeSearch from './components/RecipeSearch';
 import NutritionData from './components/NutritionData';
@@ -14,23 +14,41 @@ import {
   Route
 } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path = "/home" element={<Home />} />
-          <Route path = "/search" element={<RecipeSearch />} />
-          <Route path = "/favorite" element={<Favorite />} />
-          <Route path = "/404" element={<Page404 />} />
-          <Route path = "/nutrition" element={<NutritionData />} />
-          <Route path = "/aboutus" element={<Aboutus />} />
-          <Route path = "/recipe" element={<RecipeCard />} />
-        </Routes>
-      </Router> 
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <p className="App-Intro">{this.state.apiResponse}</p>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/search" element={<RecipeSearch />} />
+            <Route path="/favorite" element={<Favorite />} />
+            <Route path="/404" element={<Page404 />} />
+            <Route path="/nutrition" element={<NutritionData />} />
+            <Route path="/aboutus" element={<Aboutus />} />
+            <Route path="/recipe" element={<RecipeCard />} />
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default App;
+  export default App;
